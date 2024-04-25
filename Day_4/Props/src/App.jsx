8b9ad70fade5ccdff7ac8afecdbd58d669ee4367
tisha <tishaviradiya@gmail.com>
2024-04-25 10:54:14 +0530

    Day_4 task completed

---
+++ Day_4/Props/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Home from "./Components/Home.jsx"
+
+function App() {
+
+  return (
+    <>
+      <div>
+            <Home/>
+      </div>
+    </>
+  )
+}
+
+export default App
