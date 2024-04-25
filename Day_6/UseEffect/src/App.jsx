2260ffebea1c1d70c9bc80b5f5a92ca25368ee36
tisha-viradiya <3145.tisha.viradiya@gmail.com>
2024-04-25 15:11:30 +0530

    Day_6 task completed

---
+++ Day_6/UseEffect/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Home from "./components/Home"
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+          <Home/>
+      </div>
+    </>
+  )
+}
+
+export default App
