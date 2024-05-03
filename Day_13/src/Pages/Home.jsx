deb8ed5f6e4ccfee3e4bbeb41247a5f478e2f029
tisha-viradiya <3145.tisha.viradiya@gmail.com>
2024-05-03 08:20:59 +0530

    Day_13 delete task completed0

--- Day_13/src/Pages/Home.jsx
+++ Day_13/src/Pages/Home.jsx
@@ -7,7 +7,6 @@ const Home=()=>{
     const[email,setEmail]=useState("");
     const[password,setPassword]=useState("");
     const[user,setUser]=useState([]);
-
    
 
     // create
@@ -18,7 +17,7 @@ const Home=()=>{
     }
     const handleSubmit=(e)=>{
         e.preventDefault();
-        fetch('http://localhost:8000/users',{
+        fetch('http://localhost:8001/users',{
 
         method: "POST",
         headers: {
@@ -38,18 +37,32 @@ const Home=()=>{
     }
     // Read
     useEffect(()=>{
-        fetch('http://localhost:8000/users')
+        fetch('http://localhost:8001/users')
         .then((res)=>res.json())
         .then((data)=>{
             setUser(data);
         })
     },[])
+    // Delete
+    const handleDelete=(id)=>{
+        fetch(`http://localhost:8001/users/${id}`,{
+            method: "DELETE",
+        })
+        .then(()=>{
+            setUser(user.filter(user=>user.id!==id))
+        })
+        .catch((err)=>console.error(err,"something is wrong!"))
+        
+    }
+
+
     return(
         <div>
             <form onSubmit={handleSubmit}>
                 <input type="text"  onChange={(e)=>{setUserName(e.target.value)}} value={username}  placeholder="Enter your name" /><br/><br/>
                 <input type="email"  onChange={(e)=>{setEmail(e.target.value)}}    value={email}     placeholder="Enter your email"/><br/><br/>
                 <input type="password"  onChange={(e)=>{setPassword(e.target.value)}}  value={password}   placeholder="Enter your password"/><br/><br/>
+               
                 <input type="submit" />
             </form>
 
@@ -61,6 +74,7 @@ const Home=()=>{
                             <h2>{value.username}</h2>
                             <h3>{value.email}</h3>
                             <h4>{value.password}</h4>
+                            <button onClick={()=>handleDelete(value.id)}>Delete</button>
                         </div>
                     </>
                 })}
